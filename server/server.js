472987675ea08ab3123c0856aed0c8d47d25e3b9
JETAWAT SATTAYAPAN <18025868@students.latrobe.edu.au>
2018-02-09 13:30:15 +0700

    Add POST /todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req,res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+      res.send(doc);
+  }, (err) => {
+      res.status(400).send(err);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Start on port 3000');
+});
+
+
+
+//
+// var newUser = new User({
+//   email: 'olotem321@jotmailc.om'
+// });
+//
+// newUser.save().then((res) => {
+//   console.log('Save to User: ', res);
+// }, (err) => {
+//   console.log('Unable to save to DB');
+// });
+
+// var newTodo = new Todo({
+//   text: 'Cook dinner'
+// });
+
+// var newTodo = new Todo({
+//   text: soemidtjsdpoc
+//
+// })
+
+// newTodo.save().then((result) => {
+//   console.log('Save TODO: ', result);
+// }, (err) => {
+//   console.log('Unable to save TODO');
+// })
