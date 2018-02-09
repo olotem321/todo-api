f558a1e38dc922ec210413b51126ab142d0f50e2
JETAWAT SATTAYAPAN <18025868@students.latrobe.edu.au>
2018-02-09 17:05:03 +0700

    Add test cases for GET /todo/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,14 +1,18 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID,
   text: 'Fisrt test todo'
 },{
+  _id: new ObjectID,
   text: 'Second test todo'
 },{
+  _id: new ObjectID,
   text: 'Third test todo'
 }];
 
@@ -68,4 +72,31 @@ describe('GET /todos', () => {
       // console.log(res.body);
     }).end(done);
   })
+});
+
+describe('GET /todos/:id', () => {
+  it('should return to doc', (done) => {
+    request(app)
+    .get(`/todos/${todos[0]._id.toHexString()}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo.text).toBe(todos[0].text);
+    })
+    .end(done);
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    request(app)
+    .get(`/todo/${new ObjectID().toHexString()}`)
+    .expect(404)
+    .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+    .get('/todo/123')
+    .expect(404)
+    .end(done);
+  });
+
 });
