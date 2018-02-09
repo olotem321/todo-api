11b6e060c3ba46ffe006e7a20e0621612856cabc
JETAWAT SATTAYAPAN <18025868@students.latrobe.edu.au>
2018-02-09 23:31:36 +0700

    Add DELETE AND UPDATE routes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var uri = 'mongodb://olotem321:tay13211@ds231228.mlab.com:31228/todoapp';
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect( uri ||'mongodb://localhost:27017/TodoApp');
 
 module.exports ={
   mongoose
