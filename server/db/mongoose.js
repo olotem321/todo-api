6778390d6297bace1109cc0387321587617de335
JETAWAT SATTAYAPAN <18025868@students.latrobe.edu.au>
2018-02-10 11:14:24 +0700

    set URL or local and production

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,14 @@
 var mongoose = require('mongoose');
 
-var uri = 'mongodb://olotem321:tay13211@ds231228.mlab.com:31228/todoapp';
+var uri = 'mongodb://localhost:27017/TodoApp';
+
+if(process.env.NODE_ENV === 'production'){
+  uri = 'mongodb://olotem321:tay13211@ds231228.mlab.com:31228/todoapp';
+}
+
 
 mongoose.Promise = global.Promise;
-mongoose.connect(uri || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(uri);
 
 module.exports ={
   mongoose
--- server/server.js
+++ server/server.js
@@ -27,8 +27,7 @@ app.post('/todos', (req,res) => {
 app.get('/todos', (req, res) => {
   Todo.find().then((doc) => {
     res.send({
-      doc,
-      environment: process.env.NODE_ENV
+      doc
     });
   }, (err) => {
     console.log(err);
