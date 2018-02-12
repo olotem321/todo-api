e0b9c79976c2bf3ba5149ebec6f5448c2c9a0325
JETAWAT SATTAYAPAN <18025868@students.latrobe.edu.au>
2018-02-12 14:29:24 +0700

    Add GET /user/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 
@@ -110,6 +111,24 @@ app.patch('/todos/:id', (req,res) => {
     res.status(400).send();
   });
 
+});
+
+//POST /Users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(process.env.PORT, () => {
