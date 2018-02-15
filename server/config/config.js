f93b048e04c8cf49956327eaa551a713da0bf5c6
JETAWAT SATTAYAPAN <18025868@students.latrobe.edu.au>
2018-02-15 10:40:41 +0700

    Improve App config

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,14 @@
 var env = process.env.NODE_ENV || 'development';
 
-if(env === 'development'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if(env == 'test'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-} else if(process.env.NODE_ENV === 'production'){
+if(env === 'development' || env === 'test'){
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env.[key] = envConfig[key];
+  });
+
+}
+if(process.env.NODE_ENV === 'production'){
   process.env.MONGODB_URI = 'mongodb://olotem321:tay13211@ds231228.mlab.com:31228/todoapp';
 }
